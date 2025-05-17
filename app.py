f45595cf41b59558b3c6aac7fcc7667ea5509197
c04o <c04o@proton.me>
2025-05-16 17:26:47 -0700

    Main app declared

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for
+from truck_queue import Queue
+import datetime
+
+app = Flask(__name__)
+truck_queue = Queue()
+products = ["Arroz", "Frijoles", "Aceite", "Gaseosas"]
+
+@app.route('/')
+def index():
+    next_truck = truck_queue.peek()
+    return render_template('queue.html', trucks=truck_queue.items, next_truck=next_truck, products=products)
+
+@app.route('/add', methods=['POST'])
+def add_truck():
+    license = request.form.get('license')
+    product = request.form.get('product')
+    driver = request.form.get('driver')
+    company = request.form.get('company')
+    if license and product and driver and company:
+        arrival_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        truck = {
+            "license": license,
+            "product": product,
+            "driver": driver,
+            "company": company,
+            "arrival_time": arrival_time
+        }
+        truck_queue.enqueue(truck)
+    return redirect(url_for('index'))
+
+@app.route('/process', methods=['POST'])
+def process_truck():
+    truck_queue.dequeue()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
