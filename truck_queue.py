8def3d069e69b3f295d5d0994af00c6747fb2dbf
c04o <c04o@proton.me>
2025-05-16 17:27:23 -0700

    Queue class declared

---
+++ truck_queue.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.popleft()
+        return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
